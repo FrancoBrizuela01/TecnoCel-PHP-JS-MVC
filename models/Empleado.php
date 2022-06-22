0d064249f2a6fc309e5ffd52073bc144f9f917e1
Agustin <agussstiiinunez@gmail.com>
2022-06-22 20:50:23 -0300

    s

--- models/Empleado.php
+++ models/Empleado.php
@@ -13,11 +13,8 @@ class Empleado extends model
 
     public function getListaAdelantos()
     {
-<<<<<<< HEAD
-        $this->db->query("SELECT e.nombre, e.apellido, e.dni, a.fecha, a.cantidad, e.codigo_empleado
-=======
+
         $this->db->query("SELECT e.nombre, e.apellido, e.dni, a.fecha, a.cantidad, a.codigo_adelanto, e.codigo_empleado
->>>>>>> ef0e5b074d829261a590072e6c499935b08240b1
 							FROM adelantos a
 							LEFT JOIN empleados e ON a.codigo_empleado = e.codigo_empleado");
         return $this->db->fetchAll();
